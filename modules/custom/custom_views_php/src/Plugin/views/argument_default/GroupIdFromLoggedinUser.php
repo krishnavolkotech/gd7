d223db67a1132f563a29c2f78cf9e241ff8ae140
Abhishek <abhishek@azrisolutions.com>
2016-07-02 15:07:15 +0200

    Alter view result

---
+++ modules/custom/custom_views_php/src/Plugin/views/argument_default/GroupIdFromLoggedinUser.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\custom_views_php\Plugin\views\argument_default\GroupIdFromLoggedinUser.
+ */
+
+namespace Drupal\custom_views_php\Plugin\views\argument_default;
+
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Cache\CacheableDependencyInterface;
+use Drupal\Core\Plugin\Context\ContextProviderInterface;
+use Drupal\views\Plugin\views\argument_default\ArgumentDefaultPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Default argument plugin to extract a group ID.
+ *
+ * @ViewsArgumentDefault(
+ *   id = "group_id_from_url",
+ *   title = @Translation("Group ID from Loggedin User")
+ * )
+ */
+class GroupIdFromLoggedinUser extends ArgumentDefaultPluginBase implements CacheableDependencyInterface {
+
+  /**
+   * The group entity.
+   *
+   * @var \Drupal\group\Entity\GroupInterface
+   */
+  protected $group;
+
+  /**
+   * Constructs a new GroupIdFromUrl instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Plugin\Context\ContextProviderInterface $context_provider
+   *   The group route context.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ContextProviderInterface $context_provider) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    /** @var \Drupal\Core\Plugin\Context\ContextInterface[] $contexts */
+    $contexts = $context_provider->getRuntimeContexts(['group']);
+    $this->group = $contexts['group']->getContextValue();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('group.group_route_context')
+    );
+  }
+
+    /**
+   * {@inheritdoc}
+   */
+  protected static function groupIdsByMember($uid) {
+    $contents = array(
+      'closed_private-group_membership',
+      'closed-group_membership',
+      'downtimes-group_membership',
+      'group_content_type_7b308aea24fe7',
+      'group_content_type_d4b06e2b6aad0',
+      'moderate-group_membership',
+      'open-group_membership',
+      'quick_info-group_membership',
+      'group_content_type_6693a40b54133',
+      'group_content_type_c26112f8ad4cd',
+    );
+
+    $gpc = \Drupal::database()->select('group_content_field_data', 'g')
+        ->fields('g', array('gid'))
+        ->condition('uid', $uid)
+        ->condition('type', $contents, 'IN')
+        ->distinct()
+        ->execute()
+        ->fetchCol();
+    return implode('+', $gpc);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getArgument() {
+    $uid = \Drupal::currentUser()->id();
+    
+//    if (!empty($this->group) && $id = $this->group->id()) {
+//      return $id;
+//    }
+    return $this->groupIdsByMember($uid);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheMaxAge() {
+    return Cache::PERMANENT;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    // We cache the result on the route instead of the URL so that path aliases
+    // can all use the same cache context. If you look at ::getArgument() you'll
+    // see that we actually get the group ID from the route, not the URL.
+    return ['route'];
+  }
+
+}
