2b387f4583041806efa6b3401ea54f1a7a2c8d6e
sriharsha <sriharsha@azrisolutions.com>
2016-08-16 16:56:25 +0200

    new downtimes js

--- modules/custom/downtimes/js/downtimes_filter.js
+++ modules/custom/downtimes/js/downtimes_filter.js
@@ -1,9 +1,13 @@
-Drupal.behaviors.filter_form = function () {
-  $('.reset_all input').click(function() { 
-				var form_id = $(this).attr('reset_form_name'); 
-				$("."+form_id).resetForm();
-				window.location.reload();
-			      })
-  $('.start_date').datepicker({dateFormat: 'dd.mm.yy'});	
-  $('.end_date').datepicker({dateFormat: 'dd.mm.yy'});	
-}
+(function ($, Drupal) {
+    Drupal.behaviors.service_data = {
+        attach: function (context, settings) {
+            $('.reset_all input').click(function () {
+                var form_id = $(this).attr('reset_form_name');
+                $("." + form_id).resetForm();
+                window.location.reload();
+            })
+            $('.start_date').datepicker({dateFormat: 'dd.mm.yy'});
+            $('.end_date').datepicker({dateFormat: 'dd.mm.yy'});
+        }
+    };
+})(jQuery, Drupal);
