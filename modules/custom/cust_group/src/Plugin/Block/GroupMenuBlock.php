a28673f184faf05c32bfa31eaa9a7bed117283df
sandeep <sandeep@azrisolutions.com>
2016-09-02 15:39:22 +0200

    downtimes notifications

--- modules/custom/cust_group/src/Plugin/Block/GroupMenuBlock.php
+++ modules/custom/cust_group/src/Plugin/Block/GroupMenuBlock.php
@@ -22,32 +22,27 @@ class GroupMenuBlock extends BlockBase {
   public function build() {
     $menu_tree = \Drupal::menuTree();
     $group = \Drupal::routeMatch()->getParameter('group');
-    if(!is_object($group)){
-      $group = \Drupal\group\Entity\Group::load($group);
-    }
-    $oldId = $group->get('field_old_reference')->value;
-    $menu_name = 'menu-'.$oldId;
-    // Build the typical default set of menu tree parameters.
-    $parameters = $menu_tree->getCurrentRouteMenuTreeParameters($menu_name);
-
-    // Load the tree based on this set of parameters.
-    $tree = $menu_tree->load($menu_name, $parameters);
-
-    // Transform the tree using the manipulators you want.
-    /*    $manipulators = array(
-			  // Only show links that are accessible for the current user.
-			  array('callable' => 'menu.default_tree_manipulators:checkAccess'),
-			  // Use the default sorting of menu links.
-			  array('callable' => 'menu.default_tree_manipulators:generateIndexAndSort'),
-			  );*/
-    $manipulators = [];
-    $tree = $menu_tree->transform($tree, $manipulators);
-
-    // Finally, build a renderable array from the transformed tree.
-    $menu = $menu_tree->build($tree);
-
-    //    $menu_html = drupal_render($menu);
-    $title = $group->label();
-    return ['#title'=>$title,'#markup'=>\Drupal::service('renderer')->render($menu),'#cache'=>['max-age'=>0]];
+		if(!empty($group)){
+		//pr($group);exit;
+			if(!is_object($group)){
+				$group = \Drupal\group\Entity\Group::load($group);
+			}
+			$oldId = $group->get('field_old_reference')->value;
+			$menu_name = 'menu-'.$oldId;
+			// Build the typical default set of menu tree parameters.
+			$parameters = $menu_tree->getCurrentRouteMenuTreeParameters($menu_name);
+	
+			// Load the tree based on this set of parameters.
+			$tree = $menu_tree->load($menu_name, $parameters);
+			$manipulators = [];
+			$tree = $menu_tree->transform($tree, $manipulators);
+	
+			// Finally, build a renderable array from the transformed tree.
+			$menu = $menu_tree->build($tree);
+	
+			//    $menu_html = drupal_render($menu);
+			$title = $group->label();
+			return ['#title'=>$title,'#markup'=>\Drupal::service('renderer')->render($menu),'#cache'=>['max-age'=>0]];
+		}
   }
 }
--- modules/custom/cust_group/src/Routing/RouteSubscriber.php
+++ modules/custom/cust_group/src/Routing/RouteSubscriber.php
@@ -34,11 +34,13 @@ class RouteSubscriber extends RouteSubscriberBase {
 		foreach($collection as $key=>$route){
 			if (strpos($route->getPath(),'/group/{') === 0 && !in_array($key,['entity.group_content.group_membership.join_form','entity.group.canonical'		])){
 				if(in_array($key,['view.group_members_lists.page_1'])){
+					$route->setPath('/group/{group}/address/{arg_1}');
 					//as views from UI has path of kind /group/{arg_0}/address/{arg_1} 
-					$route->setRequirement('_custom_access','\Drupal\cust_group\Controller\CustNodeController::hzdGroupViewsAccess');
+					//$route->setRequirement('_custom_access','\Drupal\cust_group\Controller\CustNodeController::hzdGroupViewsAccess');
 				}else{
-					$route->setRequirement('_custom_access','\Drupal\cust_group\Controller\CustNodeController::hzdGroupAccess');
+
 				}
+				$route->setRequirement('_custom_access','\Drupal\cust_group\Controller\CustNodeController::hzdGroupAccess');
 			}
 		}
 		
