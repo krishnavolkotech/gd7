2168e0a4b007df9c2a4f4a2f5dcf57cedf7180e6
sureshk <sureshkumara@azrisolutions.com>
2016-09-22 14:03:14 +0200

    release management

--- modules/custom/hzd_release_management/src/Controller/QuickinfopublishedviewController.php
+++ modules/custom/hzd_release_management/src/Controller/QuickinfopublishedviewController.php
@@ -13,16 +13,23 @@ class QuickinfopublishedviewController extends ControllerBase {
    *  TO do : Check with shiva sir 
    */
   public function quickinfo_published_view() {
-        $group = \Drupal::routeMatch()->getParameter('group');
-        if(is_object($group)){
-          $group_id = $group->id();
-        } else {
-          $group_id = $group;
-        } 
-        
-      $node = \Drupal\node\Entity\Node::load($group_id);
-      $output['#title'] = $node->title;
-      $output = node_view($node, $view_mode = 'full', $langcode = NULL);
+//        $group = \Drupal::routeMatch()->getParameter('group');
+//        if(is_object($group)){
+//          $group_id = $group->id();
+//        } else {
+//          $group_id = $group;
+//        } 
+//      node/id/
+      $current_path = \Drupal::service('path.current')->getPath();
+      $arg = explode('/', $current_path);
+//      $result = \Drupal::service('path.alias_manager')->getAliasByPath($current_path);
+      $nid = $arg['4'];
+      $node = \Drupal\node\Entity\Node::load($nid);
+      $output = array();
+      if ($node) {
+        $output['#title'] = $node->title;
+        $output = node_view($node, $view_mode = 'full', $langcode = NULL);          
+      }
       return $output;
     }
 }
