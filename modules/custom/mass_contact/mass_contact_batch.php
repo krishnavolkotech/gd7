9ad2ce4deb3d05a071eabb3c075103b7315fd785
sriharsha <sriharsha@azrisolutions.com>
2016-09-23 10:36:19 +0200

    mass contact batch

---
+++ modules/custom/mass_contact/mass_contact_batch.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\mass_contact;
+
+use Drupal\node\Entity\Node;
+
+class MassMail {
+
+  public static function sendMail($mail, $subject, $body, &$context) {
+    $mailManager = \Drupal::service('plugin.manager.mail');
+
+    $module = 'mass_contact';
+    $to = $mail;
+    $params['message'] = $body['value'];
+    $params['subject'] = $subject;
+    $langcode = \Drupal::currentUser()->getPreferredLangcode();
+    $send = true;
+
+    $result = $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
+    if ($result['result'] !== true) {
+      drupal_set_message(t('There was a problem sending your message and it was not sent.'), 'error');
+    }
+    else {
+      drupal_set_message(t('Your message has been sent.'));
+    }
+  }
+
+  function sendMailFinished($success, $operations) {
+    // The 'success' parameter means no fatal PHP errors were detected. All
+    // other error management should be handled using 'results'.
+    /*if ($success) {
+      $message = \Drupal::translation()->formatPlural(
+          count($results), 'One post processed.', '@count posts processed.'
+      );
+    }
+    else {
+      $message = t('Finished with an error.');
+    }
+    drupal_set_message($message);*/
+    drupal_set_message('Successfuly sent the mail\'s');
+  }
+
+}
