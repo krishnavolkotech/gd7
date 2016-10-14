3b7b9841618aeb2cb671502e273138a90773bd2e
sandeep <sandeep@azrisolutions.com>
2016-10-14 19:28:14 +0200

    #15458

--- modules/contrib/group/src/Form/GroupRequestMembershipForm.php
+++ modules/contrib/group/src/Form/GroupRequestMembershipForm.php
@@ -53,24 +53,8 @@ class GroupRequestMembershipForm extends ContentEntityForm {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    parent::submitForm($form, $form_state);
-    $group = \Drupal::routeMatch()->getParameter('group');
-    $subject = "Membership Request for a Group - ".$group->label();
-    $message = $form_state->getValue('message');
-    $to = $this->config('system.site')->get('mail');
-    $mailManager = \Drupal::service('plugin.manager.mail');
-    $module = 'group';
-    $key = 'immediate_notifications';
-    $params['message'] = $message;
-    $params['subject'] = $subject;
-    $langcode = \Drupal::currentUser()->getPreferredLangcode();
-    $send = true;
-    $result = $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
-    if ($result['result'] !== true) {
-      drupal_set_message(t('There was a problem sending your message and it was not sent.'), 'error');
-    }else {
-      drupal_set_message(t('Mail sent.'), 'status');
-    }
+    //parent::submitForm($form, $form_state);
+    
   }
 
   /**
