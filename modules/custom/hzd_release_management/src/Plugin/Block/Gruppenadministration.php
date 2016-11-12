64aa4ffa9aa44aff12afe7b190bd2c48b4002816
sandeep <sandeep@azrisolutions.com>
2016-11-12 12:54:48 +0100

    group administration block access altered

--- modules/custom/hzd_release_management/src/Plugin/Block/Gruppenadministration.php
+++ modules/custom/hzd_release_management/src/Plugin/Block/Gruppenadministration.php
@@ -61,36 +61,44 @@ class Gruppenadministration extends BlockBase {
     } else {
       $groupId = $group;
     }
-    $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Inhaltsübersicht'), 'view.group_content.page_1', ['arg_0' => $groupId]);
-    $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Inhalt erstellen'), 'entity.group_content.group_node.create_page', ['group' => $groupId]);
-    $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Benutzer'), 'view.group_members.page_1', ['arg_0' => $groupId]);
-    $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Störungen und Blockzeiten'), 'downtimes.DowntimessettingForm', ['group' => $groupId]);
-    $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Bekannte Fehler und Probleme'), 'problem_management.problem_settings', ['group' => $groupId]);
-    $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Releases'), 'hzd_release_management.release_settings', ['group' => $groupId]);
-    $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Mass Contact'), 'mass_contact.bulk_mail_group_members_form');
-    if ($groupId == 32) {
-      $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Planungsdateien'), 'hzd_release_management.display_planning_files', ['group' => $groupId]);
-    }
-
-    /*    $menuHtml = '<ul class="menu nav">
-      <li><a href="/group/' . $groupId . '/content">Contents</a></li>
-      <li><a href="/group/' . $groupId . '/node/create">Content</a></li>
-      <li><a href="/group/' . $groupId . '/approved-members">Users</a></li>
-      <li><a href="/group/' . $groupId . '/downtime_settings">Disturbances and block times</a></li>
-      <li><a href="/group/' . $groupId . '/problem_settings">Known Issues</a></li>
-      <li><a href="/group/' . $groupId . '/release_settings">Releases</a></li>
-      <li><a href="/admin/group/mass_contact">Mass Contact</a></li>';
+    if(CustNodeController::isGroupAdmin($groupId)){
+      $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Inhaltsübersicht'), 'view.group_content.page_1', ['arg_0' => $groupId]);
+      $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Inhalt erstellen'), 'entity.group_content.group_node.create_page', ['group' => $groupId]);
+      $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Benutzer'), 'view.group_members.page_1', ['arg_0' => $groupId]);
+      $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Störungen und Blockzeiten'), 'downtimes.DowntimessettingForm', ['group' => $groupId]);
+      $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Bekannte Fehler und Probleme'), 'problem_management.problem_settings', ['group' => $groupId]);
+      $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Releases'), 'hzd_release_management.release_settings', ['group' => $groupId]);
+      $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Mass Contact'), 'mass_contact.bulk_mail_group_members_form');
       if ($groupId == 32) {
-      $menuHtml .= '<li><a href="/group/' . $groupId . '/planning-files">Planning Files</a></li>';
+        $menuItems[] = \Drupal\Core\Link::createFromRoute(t('Planungsdateien'), 'hzd_release_management.display_planning_files', ['group' => $groupId]);
       }
-      $menuHtml .= '</ul>'; */
-    $menuHtml = [
-      '#items' => $menuItems,
-      '#theme' => 'item_list',
-      '#list_type' => 'ul',
-      '#attributes' => ['class' => ['menu nav']],
-      '#cache' => ['max-age' => 0]
-    ];
+  
+      /*    $menuHtml = '<ul class="menu nav">
+        <li><a href="/group/' . $groupId . '/content">Contents</a></li>
+        <li><a href="/group/' . $groupId . '/node/create">Content</a></li>
+        <li><a href="/group/' . $groupId . '/approved-members">Users</a></li>
+        <li><a href="/group/' . $groupId . '/downtime_settings">Disturbances and block times</a></li>
+        <li><a href="/group/' . $groupId . '/problem_settings">Known Issues</a></li>
+        <li><a href="/group/' . $groupId . '/release_settings">Releases</a></li>
+        <li><a href="/admin/group/mass_contact">Mass Contact</a></li>';
+        if ($groupId == 32) {
+        $menuHtml .= '<li><a href="/group/' . $groupId . '/planning-files">Planning Files</a></li>';
+        }
+        $menuHtml .= '</ul>'; */
+      $menuHtml = [
+        '#items' => $menuItems,
+        '#theme' => 'item_list',
+        '#list_type' => 'ul',
+        '#attributes' => ['class' => ['menu nav']],
+        '#cache' => ['max-age' => 0]
+      ];
+    }else{
+      $menuHtml = [
+        '#markup' => '',
+        '#title'=>'',
+        '#cache' => ['max-age' => 0]
+      ];
+    }
     return $menuHtml;
   }
 
