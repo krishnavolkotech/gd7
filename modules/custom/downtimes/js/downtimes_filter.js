778f788b4c7b4412c9491d84f3e763a659149ae5
sureshk <sureshkumara@azrisolutions.com>
2016-11-29 11:08:28 +0100

    remove ajax parameter from pagination url in archived downtimes

--- modules/custom/downtimes/js/downtimes_filter.js
+++ modules/custom/downtimes/js/downtimes_filter.js
@@ -1,6 +1,6 @@
 (function ($, Drupal) {
     Drupal.behaviors.service_data = {
-        attach: function (context, settings) {
+        attach: function (context, settings) {            
             $('.reset_all').click(function () {
                 /*var form_id = $(this).attr('reset_form_name');
                  $("." + form_id).resetForm();*/
@@ -13,6 +13,13 @@
                 jQuery('#edit-string').val('');*/
                 window.location.reload();
             });
-        }
+    //    alert('test');
+        var anchor = $("#archived_search_results_wrapper #pagination > nav > ul > li > a");
+        anchor.each(function (index) {
+        var links = $(this).attr('href');
+                var new_href = links.replace('ajax_form=1&_wrapper_format=drupal_ajax', '');
+                $(this).attr('href', new_href);
+        });
+      }
     };
 })(jQuery, Drupal);
