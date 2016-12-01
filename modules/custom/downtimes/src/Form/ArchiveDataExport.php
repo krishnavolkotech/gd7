6405750fa35cec4c33ab172ec0d6d19a1ed5c2c6
sureshk <sureshkumara@azrisolutions.com>
2016-12-01 13:29:45 +0100

    archived data export maintainance resolve data export

--- modules/custom/downtimes/src/Form/ArchiveDataExport.php
+++ modules/custom/downtimes/src/Form/ArchiveDataExport.php
@@ -83,6 +83,8 @@ class ArchiveDataExport extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    
+    $group_node = 'group_node';
     $from = $form_state->getValue('from');
     $to = $form_state->getValue('to');
 
@@ -137,6 +139,7 @@ class ArchiveDataExport extends FormBase {
                     {node_field_data} n,
                     {states} s
           WHERE     ds.downtime_id = gcfa.entity_id
+          AND  gcfa.type like \'%' . $group_node . '%\'
           AND n.type = \'downtimes\'
           AND ' . $from . $to . '
           s.id = ds.state_id
@@ -144,6 +147,8 @@ class ArchiveDataExport extends FormBase {
           AND       ds.scheduled_p = 1
           AND       ds.downtime_id  = ri.downtime_id
           AND       ri.type = 1
+          AND       ds.resolved = 1
+          AND  gcfa.entity_id = n.nid
           GROUP BY 
           n.nid, s.abbr,ds.startdate_planned,ds.enddate_planned,
           ri.end_date,n.created,n.uid,ds.service_id,
@@ -153,6 +158,7 @@ class ArchiveDataExport extends FormBase {
     //CSV file headers.
     $result2[] = array('Betroffenes Land', 'Beginn', 'Voraussichtliches Ende', 'Tatsaechliches Ende', 'Gemeldet am', 'Gemeldet von', 'Verfahren', 'Status', 'Grund', 'In Wartungsfenster', 'Beschreibung');
     foreach ($result as $result1) {
+
       $result1 = (array) $result1;
       $service_id = explode(',', $result1['service_id']);
 
@@ -188,74 +194,81 @@ class ArchiveDataExport extends FormBase {
       // Making individual array to single array.
       $result2[] = $result1;
     }
-    
-    // non resolved maintenances
-    $query1 = 'SELECT group_concat(DISTINCT s.abbr SEPARATOR\', \') AS abbr,
-          ds.startdate_planned,
-          ds.enddate_planned,
-          ds.enddate_planned AS end_date,
-          n.created,
-          n.uid,
-          ds.service_id AS service_id,
-          ds.status,
-          ds.description,
-          ds.reason
-          FROM      {downtimes} ds,
-                    {group_content_field_data} oa,
-                    {node_field_data} n,
-                    {states} s
-          WHERE     ds.downtime_id = oa.entity_id
-          AND       ' . $from . $nonresolve_to . 'n.type = \'downtimes\'
-          AND       n.nid = ds.downtime_id
-          AND       s.id = ds.state_id
-          AND       ds.scheduled_p = 1
-          AND       ds.resolved != 1
-          AND       ds.cancelled != 1
-          GROUP BY  n.nid,s.abbr,ds.startdate_planned,ds.enddate_planned,ds.enddate_planned,
-                    n.created,n.uid,ds.service_id,ds.status,ds.description,ds.reason
-          ORDER BY  ds.startdate_planned ASC';
-    $result = db_query($query1)->fetchAll();
-    foreach ($result as $result3) {
-      $result3 = (array) $result3;
-      $service_id = explode(', ', $result3['service_id']);
 
-      // Getting service names from service IDs.
-      $title = array();
-      foreach ($service_id as $key => $value) {
-        $query = "select title from {node_field_data} where nid = ?";
-        $title[] = db_query($query, array($value))->fetchField();
-      }
-      $result3['uid'] = db_query("select abbr from {node_field_data} n, {cust_profile} p, {states} s where n.uid = p.uid and state_id = id and n.uid = ?", array($result3['uid']))->fetchField();
-      // Converting timestamps to Date format.
-      $result3['service_id'] = implode(', ', $title);
-      $result3['created'] = date('d.m.Y - H:i', $result3['created']);
-      $result3['startdate_planned'] = date('d.m.Y - H:i', $result3['startdate_planned']);
-      $result3['enddate_planned'] = date('d.m.Y - H:i', $result3['enddate_planned']);
-      $result3['end_date'] = date('d.m.Y - H:i', $result3['end_date']);
-      //if($result3['status'] == 'R' || $result3['reason'] == 6) {
-      if (empty($result3['reason']) || $result3['reason'] == 6) {
-        $result3['within_mw'] = 1;
-      }
-      else {
-        $result3['within_mw'] = 0;
-      }
-      $description = $result3['description'];
-      unset($result3['description']);
-      $result3['description'] = \Drupal\Core\Mail\MailFormatHelper::htmlToText($description);
-      if ($result3['reason'] > 0) {
-        #$result3['reason'] = get_reason_text($result3['reason']);
-      }
-      else {
-        $result3['reason'] = '';
-      }
-      // Making individual array to single array.
-      $result2[] = $result3;
-    }
-    $filename = "blockzeiten" . $filename_from . $filename_to . ".csv";
+//    // non resolved maintenances
+//    $query1 = 'SELECT group_concat(DISTINCT s.abbr SEPARATOR\', \') AS abbr,
+//          ds.startdate_planned,
+//          ds.enddate_planned,
+//          ds.enddate_planned AS end_date,
+//          n.created,
+//          n.uid,
+//          ds.service_id AS service_id,
+//          ds.status,
+//          ds.description,
+//          ds.reason
+//          FROM      {downtimes} ds,
+//                    {group_content_field_data} oa,
+//                    {node_field_data} n,
+//                    {states} s
+//          WHERE     ds.downtime_id = oa.entity_id
+//          AND       ' . $from . $nonresolve_to . 'n.type = \'downtimes\'
+//          AND       n.nid = ds.downtime_id
+//          AND       s.id = ds.state_id
+//          AND       ds.scheduled_p = 1
+//          AND       ds.resolved = 1
+//          AND       ds.cancelled != 1
+//          AND  oa.type like \'%' . $group_node . '%\'
+//          AND  oa.entity_id = n.nid
+//          GROUP BY  n.nid,s.abbr,ds.startdate_planned,ds.enddate_planned,ds.enddate_planned,
+//                    n.created,n.uid,ds.service_id,ds.status,ds.description,ds.reason
+//          ORDER BY  ds.startdate_planned ASC';
+//    $result = db_query($query1)->fetchAll();
+//
+//    foreach ($result as $result3) {
+//      $result3 = (array) $result3;
+//      $service_id = explode(', ', $result3['service_id']);
+//
+//      // Getting service names from service IDs.
+//      $title = array();
+//      foreach ($service_id as $key => $value) {
+//        $query = "select title from {node_field_data} where nid = ?";
+//        $title[] = db_query($query, array($value))->fetchField();
+//      }
+//      $result3['uid'] = db_query("select abbr from {node_field_data} n, {cust_profile} p, {states} s where n.uid = p.uid and state_id = id and n.uid = ?", array($result3['uid']))->fetchField();
+//      // Converting timestamps to Date format.
+//      $result3['service_id'] = implode(', ', $title);
+//      $result3['created'] = date('d.m.Y - H:i', $result3['created']);
+//      $result3['startdate_planned'] = date('d.m.Y - H:i', $result3['startdate_planned']);
+//      $result3['enddate_planned'] = date('d.m.Y - H:i', $result3['enddate_planned']);
+//      $result3['end_date'] = date('d.m.Y - H:i', $result3['end_date']);
+//      //if($result3['status'] == 'R' || $result3['reason'] == 6) {
+//      if (empty($result3['reason']) || $result3['reason'] == 6) {
+//        $result3['within_mw'] = 1;
+//      }
+//      else {
+//        $result3['within_mw'] = 0;
+//      }
+//      $description = $result3['description'];
+//      unset($result3['description']);
+//      $result3['description'] = \Drupal\Core\Mail\MailFormatHelper::htmlToText($description);
+//      if ($result3['reason'] > 0) {
+//        #$result3['reason'] = get_reason_text($result3['reason']);
+//      }
+//      else {
+//        $result3['reason'] = '';
+//      }
+//   //   $result3['nid'] = $result3['nid'];
+//      // Making individual array to single array.
+//      $result2[] = $result3;
+//    }
+//    print_r($result2);  exit;
+    
+    $filename = "geplante_blockzeiten" . $filename_from . $filename_to . ".csv";
     self::array_to_csv_download($result2, $filename);
   }
 
   function array_to_csv_download($array, $filename = "geplante_blockzeiten.csv", $delimiter = ";") {
+   //  echo '<pre>';  print_r($array);    exit();
     // open raw memory as file so no temp files needed, you might run out of memory though
     $f = fopen('php://memory', 'w');
 
