69dfbb653b942039c8af1abb3f519554df84fa1e
Sandeep Reddy <sandeep@azrisolutions.com>
2017-01-03 21:45:57 +0530

    print pdf versions

--- modules/custom/cust_pdf/src/EventSubscriber/PdfBeforeRenderSubscriber.php
+++ modules/custom/cust_pdf/src/EventSubscriber/PdfBeforeRenderSubscriber.php
@@ -32,9 +32,14 @@ class PdfBeforeRenderSubscriber implements EventSubscriberInterface {
   public function pdfPreRender(Event $event) {
       $printEngine = $event->getPrintEngine()->getDomPdf();
 //      $printEngine->configuration['default_paper_size'] = [200, 20, 595.28, 841.89];
-      $printEngine->setPaper([0, 20, 595.28, 841.89]);
+      $printEngine->setPaper(200, 20, 595.28, 841.89);
+      $printEngine->set_option('isPhpEnabled',1);
+//      $dom_pdf = $pdf->getDomPDF();
+//
+//      $canvas = $printEngine ->get_canvas();
+//      $canvas->page_text(10, 10, "Page {PAGE_NUM} of {PAGE_COUNT}", null, 10, array(0, 0, 0));
       
-    drupal_set_message('Event entity_print.print.html_alter thrown by Subscriber in module cust_pdf.', 'status', TRUE);
+//    drupal_set_message('Event entity_print.print.html_alter thrown by Subscriber in module cust_pdf.', 'status', TRUE);
   }
 
 }
