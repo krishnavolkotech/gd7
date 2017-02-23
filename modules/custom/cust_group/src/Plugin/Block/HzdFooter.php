5172f4b9fa1ae931546a6236d1810eb4fd3715d7
Sandeep Reddy <sandeep@azrisolutions.com>
2017-02-23 13:11:37 +0530

    Added Footer Html in codebase

---
+++ modules/custom/cust_group/src/Plugin/Block/HzdFooter.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\cust_group\Plugin\Block\GroupMenuBlock.
+ */
+
+namespace Drupal\cust_group\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\group\Entity\Group;
+
+/**
+ * Provides a 'cust_group' block.
+ *
+ * @Block(
+ *   id = "hzd_footer",
+ *   admin_label = @Translation("Hzd Footer content"),
+ *   category = @Translation("Custom Group")
+ * )
+ */
+class HzdFooter extends BlockBase {
+  
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $footerHtml = '<div class="row"><span class="cp col-sm-6">©2009-2016 KONSENS</span><span class="footer-links col-sm-6"><a href="#">IMPRESSUM</a> | <a href="#">DATENSCHTZ</a></span></div>
+';
+    return $footerHtml;
+  }
+  
+}
