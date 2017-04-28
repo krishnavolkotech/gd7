aae8b27546c62ba7a40d96d373a146afa11f14f1
Sandeep Reddy <sandeep@azrisolutions.com>
2017-04-28 20:15:00 +0530

    Queue Mail and upblished group feature

--- modules/custom/mass_contact/mass_contact_batch.php
+++ modules/custom/mass_contact/mass_contact_batch.php
@@ -25,7 +25,7 @@ class MassMail {
     }
   }
 
-  function sendMailFinished($success, $operations) {
+  static function sendMailFinished($success, $operations) {
     // The 'success' parameter means no fatal PHP errors were detected. All
     // other error management should be handled using 'results'.
     /*if ($success) {
