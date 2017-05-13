cb21d0f160566bda520637cf1108e65e0f6e6baa
Sandeep Reddy <sandeep@azrisolutions.com>
2017-05-13 16:27:27 +0530

    #18644

---
+++ modules/custom/cust_group/src/Form/AlteredNodeForm.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Drupal\cust_group\Form;
+
+use Drupal\node\NodeForm;
+
+/**
+ * Description of AlteredNodeForm
+ *
+ * @author sandeep
+ */
+class AlteredNodeForm extends NodeForm {
+
+  //put your code here
+
+  public function save(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    parent::save($form, $form_state);
+    $messages = drupal_get_messages();
+    $node = $this->entity;
+    if ($node->getType() == 'quickinfo') {
+      if ($node->get('status')->value == 1) {
+        drupal_set_message(t('@type %title has been published.', ['@type' => node_get_type_label($node), '%title' => $node->tolink($node->label())]));
+      } else {
+        drupal_set_message(t('@type %title has been saved.', ['@type' => node_get_type_label($node), '%title' => $node->tolink($node->label())]));
+      }
+    }
+  }
+
+}
