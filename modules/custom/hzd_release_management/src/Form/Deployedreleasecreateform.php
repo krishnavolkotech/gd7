95490f51c259a8a2b1730613581d1ac79705e28d
Sandeep Reddy <sandeep@azrisolutions.com>
2017-06-09 15:18:51 +0530

    #18982

--- modules/custom/hzd_release_management/src/Form/Deployedreleasecreateform.php
+++ modules/custom/hzd_release_management/src/Form/Deployedreleasecreateform.php
@@ -107,7 +107,7 @@ class Deployedreleasecreateform extends FormBase {
       '#maxlength' => '20',
       '#attributes' => array("class" => ["deployed_date"]),
       '#weight' => -3,
-        '#placeholder'=> t('< Date >'),
+        '#placeholder'=> '<'.t('Date')->render().'>',
 
     );
 
@@ -188,7 +188,7 @@ class Deployedreleasecreateform extends FormBase {
       $default_releases = get_undeployed_dependent_release($service, $environment);
     }
     else {
-      $default_releases = array("0" => t('Release'));
+      $default_releases = array("0" => '<'.t('Release')->render().'>');
     }
 
 //    $form['deployed_releases']['#options'] =$default_releases;
