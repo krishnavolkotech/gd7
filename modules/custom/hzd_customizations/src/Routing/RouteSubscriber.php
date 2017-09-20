f09a3648585eaabccceb6b97b372d44096b0d54f
sandeep <sandeep@azrisolutions.com>
2017-09-20 13:16:08 +0200

    #21219 added support for downtimes list and archive list

--- modules/custom/hzd_customizations/src/Routing/RouteSubscriber.php
+++ modules/custom/hzd_customizations/src/Routing/RouteSubscriber.php
@@ -60,7 +60,10 @@ class RouteSubscriber extends RouteSubscriberBase {
     $response = $event->getResponse();
     $request = $event->getRequest();
     $node = $request->get($key);
-    if ($node && $node instanceof \Drupal\node\NodeInterface && $node->getType() == "downtimes") {
+    $route = \Drupal::routeMatch()->getRouteName();
+    if(in_array($route,['downtimes.new_downtimes_controller_newDowntimes','downtimes.archived_downtimes_controller'])) {
+	$response->headers->set('X-Frame-Options', 'ALLOWALL');
+    }elseif ($node && $node instanceof \Drupal\node\NodeInterface && $node->getType() == "downtimes") {
       $response->headers->set('X-Frame-Options', 'ALLOWALL');
     }
   }
