ef24b16abda253ebb32e6a83c5442f5e07a13ff6
Sandeep Reddy <sandeep@azrisolutions.com>
2018-03-09 16:19:21 +0530

    Error while node edit due to conflicts.

--- modules/custom/cust_group/src/CustGroupHelper.php
+++ modules/custom/cust_group/src/CustGroupHelper.php
@@ -13,6 +13,7 @@ use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\node\Entity\Node;
 use Drupal\group\Entity\GroupContentType;
 use Drupal\Core\Url;
+use Drupal\Core\Form\FormState;
 
 /**
  * Description of CustGroupHelper
