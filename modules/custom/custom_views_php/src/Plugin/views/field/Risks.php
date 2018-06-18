d95399f209cae1c407278e16305ac3b95f976e7c
Denis Roy @ HZD <denis@azrisolutions.com>
2018-06-18 13:30:06 +0200

    #24437 Added Risk Management

---
+++ modules/custom/custom_views_php/src/Plugin/views/field/Risks.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\custom_views_php\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Drupal\Core\Url;
+
+/**
+ * A handler to provide a field that is completely custom by the administrator.
+ *
+ * @ingroup views_field_handler
+ *
+ * @ViewsField("risk_cluster_risks")
+ */
+class Risks extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function usesGroupBy() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Do nothing -- to override the parent query.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+
+    $options['hide_alter_empty'] = array('default' => FALSE);
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $cluster = $values->_entity;
+    $risks = \Drupal::entityTypeManager()
+    ->getStorage('node')
+    ->loadByProperties(['field_risk_clusters' => $cluster->getEntity()->id(),'type'=>'risk']);
+    $items = array_map(function($risk){
+      return $risk->get('field_id')->value;
+    },$risks);
+    return implode(', ',$items);
+  }
+
+}
---
+++ modules/custom/hzd_risk_management/src/Plugin/Field/FieldFormatter/TokenizedFieldFormatter.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\hzd_risk_management\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceFormatterBase;
+
+/**
+ * Plugin implementation of the 'tokenized_field_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "tokenized_field_formatter",
+ *   label = @Translation("Tokenized field formatter"),
+ *   field_types = {
+ *     "entity_reference"
+ *   }
+ * )
+ */
+class TokenizedFieldFormatter extends EntityReferenceFormatterBase{
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+	'token_data'=>""
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form['token_data'] = ['#type'=>'textfield','#title'=>t('Token to generate field markup'), '#default_value'=>$this->getSetting('token_data')];
+    if(\Drupal::moduleHandler()->moduleExists('token')) {
+      $form['token_tree'] = [
+        '#theme' => 'token_tree_link',
+        '#token_types' => 'all',
+        '#show_restricted' => TRUE,
+      ];
+    }
+  return $form
+     + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    $data = $this->getSetting('token_data');
+    $token_service = \Drupal::token();
+    foreach ($this->getEntitiesToView($items, $langcode) as $delta => $item) {
+      $elements[$delta] = ['#markup'=>$token_service->replace($data, array('node' => $item))];
+    }
+    return $elements;
+
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    // The text value has no text format assigned to it, so the user input
+    // should equal the output, including newlines.
+    return nl2br(Html::escape($item->value));
+  }
+
+}
