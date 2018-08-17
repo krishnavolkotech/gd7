7cfbc70cf896c7bfe67971b7fc0238b7005b047c
abhishek <abhishek@azrisolutions.com>
2018-08-17 14:28:15 +0530

    Added validation to avoide blank update #24963

--- modules/custom/cust_group/src/Form/Imupdateticket.php
+++ modules/custom/cust_group/src/Form/Imupdateticket.php
@@ -7,7 +7,7 @@ use Drupal\Core\Form\FormStateInterface;
 
 class Imupdateticket extends FormBase {
     private static $slug = 0;
-    
+
     /**
      * {@inheritdoc}
      */
@@ -15,14 +15,14 @@ class Imupdateticket extends FormBase {
         self::$slug += 1;
         return 'im_update_ticket_' . self::$slug;
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(array $form, FormStateInterface $form_state, $parameter = NULL) {
       $form['ticket_id'] = [
         '#type' => 'textfield',
-        '#default_value' => isset($parameter['ticket']) ? $parameter['ticket'] : '',
+        '#default_value' => isset($parameter['ticket']) ? $parameter['ticket'] : NULL,
         '#size' => 60,
         ];
 
@@ -39,12 +39,23 @@ class Imupdateticket extends FormBase {
       $form['#attributes'] = ['class' => ['hide', 'ticket-update-form']];
       return $form;
     }
-    
+
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+    $values = $form_state->getValues();
+
+      if (empty(trim($values['ticket_id']))) {
+        drupal_set_message($this->t("Please enter ticket id."), 'error');
+        $form_state->setErrorByName('ticket_id',t('Please enter ticket id.'));
+      }
+
+  }
+
     /**
      * {@inheritdoc}
      */
     public function submitForm(array &$form, FormStateInterface $form_state) {
-      $ticket_id = $form_state->getValue('ticket_id');
+      $ticket_id = empty(trim($form_state->getValue('ticket_id'))) ? NULL : $form_state->getValue('ticket_id');
       $file_id = $form_state->getValue('file_id');
       if (isset($ticket_id) && isset($file_id)) {
         $query = \Drupal::entityQuery('cust_group_imattachments_data');
