c2797cd4d769a05e7aa68bf387d88e9cda127f21
abhishek <abhishek@azrisolutions.com>
2018-10-03 14:51:29 +0530

    Updating effect #25055

--- modules/custom/cust_group/js/cust_group.js
+++ modules/custom/cust_group/js/cust_group.js
@@ -15,13 +15,13 @@
                 }
             });
 
-            var acc = document.getElementsByTagName('caption');
+            var acc = document.getElementsByClassName('faq-title');
             var i;
             for (i = 0; i < acc.length; i++) {
                 acc[i].addEventListener("click", function () {
                     this.classList.toggle("active");
                     var panel = this.nextElementSibling;
-                    $(panel).toggle("fast");
+                    $(panel).slideToggle("slow");
                 });
             }
         }
