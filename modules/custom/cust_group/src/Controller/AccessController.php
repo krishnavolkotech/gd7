ab7feea2ba3d44703cf1489ae8b43fb7ffc5ae8b
Abhishek <abhishek@azrisolutions.com>
2019-03-15 14:50:01 +0530

    Fixed issue for autoren-rz-schnellinfo #25629

--- modules/custom/cust_group/src/Controller/AccessController.php
+++ modules/custom/cust_group/src/Controller/AccessController.php
@@ -196,10 +196,10 @@ class AccessController extends ControllerBase
                 $releaseGroup = Group::load(RELEASE_MANAGEMENT);
                 $releaseMember = $releaseGroup->getMember($user);
 //                pr($releaseMember->id());exit;
-                if (!$content || !$releaseMember) {
+                if (!$content && !$releaseMember) {
                     return AccessResult::forbidden();
                 } else {
-                    if (!$node->isPublished() && $content->getGroupContent()->get('request_status')->value == 1) {
+                    if (($content !== FALSE) && !$node->isPublished() && $content->getGroupContent()->get('request_status')->value == 1) {
                         return AccessResult::allowed();
                     }
                 }
