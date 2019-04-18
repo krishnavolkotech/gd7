d77064397dbe342bf78fee3a3972464f10548fa8
Abhishek <abhishek@azrisolutions.com>
2019-04-18 16:57:56 +0530

    Fixing pagination issue #25708

--- modules/custom/cust_group/src/Controller/FrontPageController.php
+++ modules/custom/cust_group/src/Controller/FrontPageController.php
@@ -9,7 +9,7 @@ use Drupal\front\Controller\FrontPage;
 /**
  * This Class is used to override front page controller from front contrib module.
  *
- * 
+ *
  */
 class FrontPageController extends ControllerBase {
 
@@ -22,14 +22,26 @@ class FrontPageController extends ControllerBase {
     // Return only anonymous front page content and nothing for authenticated user.
     if ($currentUser->isAnonymous()) {
       return $front_page_obj->view();
-    }
-    else {
+    } else {
+      $view = Drupal\views\Views::getView('problems_front_page');
+      $view->setDisplay('block_1');
+      $view->execute();
+      $resultall = [];
+      if (empty($view->build_info['fail']) and empty($view->build_info['denied'])) {
+        $result = $view->result;
+        $resultall = [];
+        foreach ($result as $nid) {
+          $resultall[] = $nid->nid;
+        }
+      }
+      $tempstore = \Drupal::service('user.private_tempstore')->get('problem_management');
+      $tempstore->set('problem_paginations', implode(',', $resultall));
       return [
         '#type' => 'markup',
         '#markup' => '',
         '#title' => '',
       ];
-    }    
+    }
   }
 
 }
