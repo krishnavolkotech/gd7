6d7eeba2db067fc65d6b9f786a16fe2de54a953c
Narsing <narsing@azrisolutions.com>
2019-11-22 16:27:36 +0530

    added function for fetch all user groups #26324

--- modules/custom/cust_group/src/CustGroupHelper.php
+++ modules/custom/cust_group/src/CustGroupHelper.php
@@ -145,4 +145,13 @@ class CustGroupHelper {
     return $element;
   }
   
+  public static function hzd_user_groups_list($uid) {
+    $user_groups_query = \Drupal::database()->query("SELECT gfd.id, gfd.label FROM {groups_field_data} gfd, {group_content_field_data} gcfd 
+                   WHERE gcfd.request_status = 1 AND gfd.id = gcfd.gid  AND gcfd.entity_id = :eid ORDER BY gfd.label", array(":eid" => $uid))->fetchAll();
+      $user_groups = array();
+      foreach ($user_groups_query as $groups_list) {
+          $user_groups[$groups_list->id] = $groups_list->label;
+      }
+      return $user_groups;
+  }
 }
