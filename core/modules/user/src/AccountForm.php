afebeafe5319cda9603709e1935cdd97e949645b
Narsing <narsing@azrisolutions.com>
2020-03-31 16:21:00 +0530

    updated drupal to 8.8.4 #26789

--- core/modules/user/src/AccountForm.php
+++ core/modules/user/src/AccountForm.php
@@ -115,8 +115,7 @@ abstract class AccountForm extends ContentEntityForm implements TrustedCallbackI
         'spellcheck' => 'false',
       ],
       '#default_value' => (!$register ? $account->getAccountName() : ''),
-      '#access' => ($register || ($user->id() == $account->id() && $user->hasPermission('change own username')) || $admin),
-      //'#access' => $account->name->access('edit'),
+      '#access' => $account->name->access('edit'),
     ];
 
     // Display password field only for existing users or when user is allowed to
