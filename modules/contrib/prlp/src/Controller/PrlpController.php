99a1bfc361187d051aef2aea9ba68e806290d033
Narsing <narsing@azrisolutions.com>
2020-06-03 17:33:29 +0530

    updated prlp module #28113

--- modules/contrib/prlp/src/Controller/PrlpController.php
+++ modules/contrib/prlp/src/Controller/PrlpController.php
@@ -2,13 +2,10 @@
 
 namespace Drupal\prlp\Controller;
 
-use Drupal;
-use Drupal\Component\Utility\Crypt;
-use Drupal\Core\Form\FormState;
+use Drupal\Core\Url;
 use Drupal\user\Controller\UserController;
-use Drupal\user\Form\UserPasswordResetForm;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Controller routines for prlp routes.
@@ -16,8 +13,7 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 class PrlpController extends UserController {
 
   /**
-   * Override resetPassLogin() method from parent object to validate and save
-   * new password with.
+   * Override resetPassLogin() to redirect to the configured path.
    *
    * @param \Symfony\Component\HttpFoundation\Request $request
    *   The request.
@@ -32,69 +28,54 @@ class PrlpController extends UserController {
    *   Returns parent result object.
    */
   public function prlpResetPassLogin(Request $request, $uid, $timestamp, $hash) {
-    // The current user is not logged in, so check the parameters.
-    $current = REQUEST_TIME;
-    /** @var \Drupal\user\UserInterface $user */
-    $user = $this->userStorage->load($uid);
+    $response = parent::resetPassLogin($uid, $timestamp, $hash);
 
-    // Check if the hash is valid, but not if you were told by prlp to
-    // skip checking it.
-    $check_hash = !Drupal::request()->getSession()->get('prlp_skip_hash_check');
-    // remove this session setting immediately to avoid security holes
-    !Drupal::request()->getSession()->remove('prlp_skip_hash_check');
-    $invalid_hash = $check_hash && !Crypt::hashEquals($hash, user_pass_rehash($user, $timestamp));
-    // Verify that the user exists and is active.
-    if ($user === NULL || !$user->isActive() || $invalid_hash) {
-      // Blocked or invalid user ID, so deny access. The parameters will be in
-      // the watchdog's URL for the administrator to check.
-      throw new AccessDeniedHttpException();
-    }
+    try {
+      // Deconstruct the redirect url from the response.
+      $parsed_url = parse_url($response->getTargetUrl());
+      $response_route = Url::fromUserInput($parsed_url['path']);
 
-    if ($request->getMethod() == 'POST') {
-      // Build form to call for validation and submit handlers.
-      $timeout = $this->config('user.settings')->get('password_reset_timeout');
-      // No time out for first time login.
-      if (($user->getLastLoginTime() && $user->getLastLoginTime() > $timestamp) || $current - $timestamp > $timeout) {
-        drupal_set_message($this->t('You have tried to use a one-time login link that has expired. Please request a new one using the form below.'), 'error');
-        return $this->redirect('user.pass');
+      // Check that the response route matches the "success" route from core and
+      // if it does apply the password change and update the redirect
+      // destination.
+      if ($response_route && $response_route->getRouteName() == 'entity.user.edit_form') {
+        if ($request->request->has('pass') && $passwords = $request->request->get('pass')) {
+          // $passwords should be an array, if that's not the case nothing
+          // should be done to the user.
+          $pass = is_array($passwords) ? reset($passwords) : NULL;
+          if (!empty($pass)) {
+            /** @var \Drupal\user\UserInterface $user */
+            $user = $this->userStorage->load($uid);
+            $user->setPassword($pass);
+            $user->save();
+            $this->messenger()->addStatus($this->t('Your new password has been saved.'));
+          }
+        }
+
+        $login_destination = $this->config('prlp.settings')->get('login_destination');
+        if (!$login_destination) {
+          $login_destination = '/user/%user/edit';
+        }
+        $login_destination = str_replace('%user', $uid, $login_destination);
+        $login_destination = str_replace('%front', $this->config('system.site')->get('page.front'), $login_destination);
+        if (substr($login_destination, 0, 1) !== '/') {
+          $login_destination = '/' . $login_destination;
+        }
+
+        return new RedirectResponse($login_destination);
       }
-      $expiration_date = $user->getLastLoginTime() ? $this->dateFormatter->format($timestamp + $timeout) : NULL;
-      $form_state = new FormState();
-      $form_state->addBuildInfo('args', array_values([
-        $user, $expiration_date, $timestamp, $hash
-      ]));
-      $this->formBuilder()->buildForm(UserPasswordResetForm::class, $form_state);
-
-      $session = $request->getSession();
-      $session->set('pass_reset_hash', $hash);
-      $session->set('pass_reset_timeout', $timestamp);
-      return $this->redirect(
-        'user.reset.form',
-        ['uid' => $uid]
-      );
+    }
+    catch (\InvalidArgumentException $exception) {
+      // This exception is an edge case scenario thrown by Url::fromUserInput()
+      // Should fromUserInput() throw this treat it as a failed authentication
+      // and log the user out then clear success messages and add a failure
+      // message.
+      user_logout();
+      $this->messenger()->deleteAll();
+      $this->messenger()->addError($this->t('You have tried to use a one-time login link that has either been used or is no longer valid. Please request a new one using the form below.'));
     }
 
-    // Time out, in seconds, until login URL expires.
-    $timeout = $this->config('user.settings')->get('password_reset_timeout');
-    // No time out for first time login.
-    if ($user->getLastLoginTime() && $current - $timestamp > $timeout) {
-      drupal_set_message($this->t('You have tried to use a one-time login link that has expired. Please request a new one using the form below.'), 'error');
-      return $this->redirect('user.pass');
-    }
-    elseif ($user->isAuthenticated() && ($timestamp >= $user->getLastLoginTime()) && ($timestamp <= $current)) {
-      user_login_finalize($user);
-      $this->logger->notice('User %name used one-time login link and changed his password at time %timestamp.', ['%name' => $user->getDisplayName(), '%timestamp' => $timestamp]);
-      drupal_set_message($this->t('You have just used your one-time login link. It is no longer necessary to use this link to log in.'));
-      // Let the user's password be changed without the current password check.
-      $token = Crypt::randomBytesBase64(55);
-      $_SESSION['pass_reset_' . $user->id()] = $token;
-      return $this->redirect(
-        'entity.user.edit_form',
-        ['user' => $user->id()]
-      );
-    }
-
-    drupal_set_message($this->t('You have tried to use a one-time login link that has either been used or is no longer valid. Please request a new one using the form below.'), 'error');
     return $this->redirect('user.pass');
   }
+
 }
--- modules/contrib/prlp/src/Form/AdminSettingsForm.php
+++ modules/contrib/prlp/src/Form/AdminSettingsForm.php
@@ -47,7 +47,7 @@ class AdminSettingsForm extends ConfigFormBase {
     $form['login_destination'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Login Destination'),
-      '#description' => $this->t('User will be taken to this path after they log in with the password reset link. Token %uid can be used in the path, and will be replaced with the uid of the current user. Use %front for site front-page.'),
+      '#description' => $this->t('User will be taken to this path after they log in with the password reset link. Token %user can be used in the path, and will be replaced with the uid of the current user. Use %front for site front-page.'),
       // '#maxlength' => 64,
       // '#size' => 64,
       '#default_value' => $config->get('login_destination'),
