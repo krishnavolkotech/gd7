6b29d924ad7222f19888d3321c1125fd5b3b92b0
narsing <narsing@azrisolutions.com>
2021-02-22 11:12:26 +0100

    fixed notices #29004

--- modules/contrib/group/src/Plugin/views/argument/GroupId.php
+++ modules/contrib/group/src/Plugin/views/argument/GroupId.php
@@ -54,8 +54,8 @@ class GroupId extends NumericArgument {
    */
   public function titleQuery() {
     $titles = [];
-
-    $groups = $this->groupStorage->loadMultiple($this->value);
+    $values = is_array($this->value[0])?$this->value[0]:$this->value;
+    $groups = $this->groupStorage->loadMultiple($values);
     foreach ($groups as $group) {
       $titles[] = $group->label();
     }
