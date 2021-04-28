ce213986e0a0c63515eb9f2856a403b5725a9fa4
Robin Schomburg <Robin.Schomburg@hzd.hessen.de>
2021-04-28 09:11:27 +0200

    HZD React Modul Basis

---
+++ modules/custom/hzd_react/src/Controller/ReactController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\hzd_react\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Defines ReactController class.
+ */
+class ReactController extends ControllerBase {
+
+  /**
+   * Display the markup.
+   *
+   * @return array
+   *  Return markup array.
+   */
+  public function content($reactPage = NULL) {
+    return [
+      '#type' => 'markup',
+      '#markup' => '<div id="react-app"></div>',
+      '#attached' => ['library' => 'hzd_react/react_app_dev']
+    ];
+  }
+}
