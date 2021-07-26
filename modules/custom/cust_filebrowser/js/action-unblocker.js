bad8b123485029ac55f1d821b01b31fc160d317f
Robin Schomburg <robin.schomburg@hzd.hessen.de>
2021-07-26 17:26:52 +0200

    Filebrowser: Sorting and Filtering.

--- modules/custom/cust_filebrowser/js/action-unblocker.js
+++ modules/custom/cust_filebrowser/js/action-unblocker.js
@@ -7,4 +7,90 @@
       });
     }
   };
+
+  Drupal.behaviors.filebrowserFilter = {
+    attach: function (context, settings) {
+      $("#form-action-actions-wrapper").once('cust_filebrowser').after('<input class="form-control" id="myInput" type="text" placeholder="Suche...">');
+      $("#myInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#edit-table tbody tr").filter(function () {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+    }
+  };
+
+  Drupal.behaviors.filebrowserTablesorter = {
+    attach: function (context, settings) {
+      // Your custom JavaScript goes inside this function ...
+
+      // Without this, headers are not sortable for regular users. Why?
+      $.fn.admin_toolbar = function () {
+        if ($('#toolbar-administration').length) {
+          return 80;
+        } else {
+          return 0;
+        }
+      }
+
+      // custom date parser for tablesorter, based on release_management.js
+      $.tablesorter.addParser({
+        // set a unique id
+        id: 'date-sorter',
+        is: function (s) {
+          // return false so this parser is not auto detected
+          return false;
+        },
+        format: function (s, table) {
+          // format your data for normalization 
+          if (s) {
+            var mydatetime = s.split(' - ');
+            if (mydatetime[0] && mydatetime[1]) {
+              var dateele = mydatetime[0].split('.');
+              var timeele = mydatetime[1].split(':');
+              
+              //adding 20 if date is formatted in only YY format.
+              if (dateele[2].length == 2) {
+                dateele[2] = '20' + dateele[2];
+              }
+              // new Date(year, monthIndex [, day [, hour [, minutes [, seconds [, milliseconds]]]]]);
+              let unixtime = new Date(
+                parseInt(dateele[2]), // Year
+                parseInt(dateele[1]), // Month
+                parseInt(dateele[0]), // Day
+                parseInt(timeele[0]), // Hour
+                parseInt(timeele[1])  // Minutes
+              )
+              .getTime() / 1000;
+              return unixtime;
+            }
+          }
+        },
+        // set type, either numeric or text
+        type: 'numeric'
+      });
+
+      $('#edit-table').tablesorter({
+        headers: {
+          0: { sorter: false },
+          3: { sorter: 'date-sorter' },
+        },
+        showProcessing: true,
+        headerTemplate: '{content} {icon}',
+        widgets: ['zebra', 'stickyHeaders'],
+        widgetOptions: {
+          stickyHeaders: 'sticky-header',
+          stickyHeaders_offset: $.fn.admin_toolbar(),
+          stickyHeaders_cloneId: '-sticky',
+          stickyHeaders_addResizeEvent: true,
+          stickyHeaders_includeCaption: true,
+          stickyHeaders_zIndex: 2,
+          stickyHeaders_attachTo: null,
+          stickyHeaders_xScroll: null,
+          stickyHeaders_yScroll: null,
+          stickyHeaders_filteredToTop: true
+        }
+      });
+    }
+  };
 })(jQuery, Drupal);
