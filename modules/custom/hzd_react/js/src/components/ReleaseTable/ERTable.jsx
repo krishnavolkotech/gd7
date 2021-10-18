2c6d542fcd3b49bc593f3197a515897c40b0fe44
mareile <mareile.moshammer@hzd.hessen.de>
2021-10-18 12:12:25 +0200

    Formatierung & Kommentare Eingesetzte Uebersicht

--- modules/custom/hzd_react/js/src/components/ReleaseTable/ERTable.jsx
+++ modules/custom/hzd_react/js/src/components/ReleaseTable/ERTable.jsx
@@ -3,17 +3,20 @@ import { Table} from 'react-bootstrap';
 
 function ERTable({ eingesetzte, landGefiltert, verfahrenGefiltert}) {
 
-
   var verfahrenKurz;
   verfahrenKurz = verfahrenGefiltert;
   verfahrenKurz.sort();
 
   var landKurz;
-  landKurz=landGefiltert;
+  landKurz = landGefiltert;
+
 
   let dtable = {};
+    // For each service add object to dtable.
   for (let i = 0; i < verfahrenKurz.length; i++) {
     dtable[verfahrenKurz[i]] = {};
+
+    // Deployed releases filtered by service and land. result is added to dtable.
     for (let j = 0; j < landKurz.length; j++) {
       let gefiltert = eingesetzte.filter(eingesetzt => {
         let result = true;
@@ -26,87 +29,80 @@ function ERTable({ eingesetzte, landGefiltert, verfahrenGefiltert}) {
         return result;
       });
 
+      // Creates list of matching release titles.
       let namen = [];
       for (let k = 0; k < gefiltert.length; k++) {
         namen.push(gefiltert[k].release);
       }
-      // if (namen.length == 0) {
-      //   continue;
-      // }
-      
       dtable[verfahrenKurz[i]][landKurz[j]] = namen;
     }
   }
 
   var ids2 = [];
-      ids2[2] = ['BW'];
-      ids2[3] = ['BY'];
-      ids2[4] = ['BE'];
-      ids2[5] = ['BB'];
-      ids2[6] = ['HB'];
-      ids2[7] = ['HH'];
-      ids2[8] = ['HE'];
-      ids2[9] = ['MV'];
-      ids2[10] = ['NI'];
-      ids2[11] = ['NW'];
-      ids2[12] = ['RP'];
-      ids2[13] = ['SL'];
-      ids2[14] = ['SN'];
-      ids2[15] = ['ST'];
-      ids2[16] = ['SH'];
-      ids2[17] = ['TH'];
-      ids2[18] = ['BU'];
+    ids2[2] = ['BW'];
+    ids2[3] = ['BY'];
+    ids2[4] = ['BE'];
+    ids2[5] = ['BB'];
+    ids2[6] = ['HB'];
+    ids2[7] = ['HH'];
+    ids2[8] = ['HE'];
+    ids2[9] = ['MV'];
+    ids2[10] = ['NI'];
+    ids2[11] = ['NW'];
+    ids2[12] = ['RP'];
+    ids2[13] = ['SL'];
+    ids2[14] = ['SN'];
+    ids2[15] = ['ST'];
+    ids2[16] = ['SH'];
+    ids2[17] = ['TH'];
+    ids2[18] = ['BU'];
 
   const tableHeader = (
-    <thead  class="sticky-row ">
-        <tr  >
-          <th >Verfahren</th>
+    <thead  class="sticky-row">
+      <tr>
+        <th>Verfahren</th>
           { landKurz.map((land) => {
             return (
               <th>{ids2[land]}</th>
             );
           })}
-        </tr>
-        </thead>
+      </tr>
+    </thead>
   );
   
-
   const tableBody = buildTableBody();
 
-  function buildTableBody () {
-
-    let result =[];
-
-  
+  function buildTableBody() {
+    let result = [];
+    // For each key (=service) in dtable create a new row with service name as first cell.
     for (const key in dtable) {
-      let row =[<td class="sticky-col" >{key}</td>];
+      let row = [<td class="sticky-col" >{key}</td>];
       var len = Object.keys(dtable[key]).length;
+      // For each key(=land) in dtable[key] create list with maching releases.
       for (const land in dtable[key]) {
-        
+        // Add different class to td element if it is the last land to be added.
         if (Object.keys(dtable[key]).indexOf(land) !== len - 1) {
-        let list = dtable[key][land].map(release => <li>{release}</li>)
-        row.push(<td class="deployed_overview_cell"><ul>{list}</ul></td>);
+          let list = dtable[key][land].map(release => <li>{release}</li>)
+          row.push(<td class="deployed_overview_cell"><ul>{list}</ul></td>);
+        }
+        if (Object.keys(dtable[key]).indexOf(land) == len - 1) {
+          let list = dtable[key][land].map(release => <li>{release}</li>)
+          row.push(<td class="deployed_overview_last_cell"><ul>{list}</ul></td>);
+        }
       }
-      if (Object.keys(dtable[key]).indexOf(land) == len - 1) {
-        let list = dtable[key][land].map(release => <li>{release}</li>)
-        row.push(<td class="deployed_overview_last_cell"  ><ul>{list}</ul></td>);
-      }
-    }
-      result.push(<tr  >{row}</tr>);
+      result.push(<tr>{row}</tr>);
     }
     return result;
-   
   }
-
 
   return(
     <div >
-    <Table bsClass="sticky-table table deployed_overview_table">
-    {tableHeader}
-     <tbody >
-     {tableBody}
-     </tbody >
-    </Table>
+      <Table bsClass="sticky-table table deployed_overview_table">
+        {tableHeader}
+        <tbody >
+          {tableBody}
+        </tbody >
+      </Table>
     </div>
   );
 }
--- modules/custom/hzd_react/js/src/components/ReleaseTable/Ers.jsx
+++ modules/custom/hzd_react/js/src/components/ReleaseTable/Ers.jsx
@@ -1,115 +1,101 @@
 import React, { useState, useEffect }from 'react';
 import ERTable from './ERTable';
 import ERFilter from './ERFilter';
-import ReleaseNav from './ReleaseNav';
-
 
 function Ers() {
 
+   /** @const {array} eingesetzte - Array that contains all deployed releases as ob. */
   const [eingesetzte, setEingesetzte] = useState([]);
 
+   /** @const {array} landFilter A filter which shows the selected options for Land. */
   const [landFilter, setLandFilter] = useState([]);
 
+   /** @const {array} landGefiltert - Contains only the Länder selected. */
   const [landGefiltert, setLandGefiltert] = useState([]);
 
+  /** @const {array} verfahrenFilter A filter which shows the selected options for Services. */
   const [verfahrenFilter, setVerfahrenFilter] = useState([]);
 
+   /** @const {array} verfahrenGefiltert - Contains the Services selected. */
   const [verfahrenGefiltert, setVerfahrenGefiltert] = useState([]);
 
+  /** @const {string} typeFilter - Default value for the Type Filter is (459 = KONSENS) */
   const [typeFilter, setTypeFilter] = useState("459");
 
+  /** @const {object} checkedLand - Object with an property for each Land. Default = no Land is selected in the Filter &all properties = false */
   const [checkedLand, setCheckedLand] = useState({
-      2:false,
-      3:false,
-      4:false,
-      5:false,
-      6:false,
-      7:false,
-      8:false,
-      9:false,
-      10:false,
-      11:false,
-      12:false,
-      13:false,
-      14:false,
-      15:false,
-      16:false,
-      17:false,
-      18:false,
-
+    2:false,
+    3:false,
+    4:false,
+    5:false,
+    6:false,
+    7:false,
+    8:false,
+    9:false,
+    10:false,
+    11:false,
+    12:false,
+    13:false,
+    14:false,
+    15:false,
+    16:false,
+    17:false,
+    18:false,
   });
 
+  /** @const {array} checkedServices - Shows which checkboxes of the Services options are checked. */
   const [checkedServices, setCheckedServices] = useState([]);
 
+   /** @const {boolean} show - Indicates wheather the Modal with the filter options is shown, default = not shown */
   const [show, setShow] = useState(false);
 
- 
-
-  useEffect(() => {  
-  let url = "/api/v1/deployments?status[]=1&environment=1&items_per_page=All&type=" + typeFilter ;
-
-
-  fetch(url)
-  .then(results=> results.json())
-  .then(post=> setEingesetzte(post));
-
+  //Get all deployed releases of the selected type.
+  useEffect(() => {
+    let url = "/api/v1/deployments?status[]=1&environment=1&items_per_page=All&type=" + typeFilter ;
+    fetch(url)
+      .then(results=> results.json())
+      .then(post=> setEingesetzte(post));
   }, [typeFilter]);
 
 
- 
-
-
   function applyLandFilter() {
-    const LandAlle=[];
-    eingesetzte.map(eingesetzt => LandAlle[LandAlle.length] = eingesetzt.state) ;
-    // doppelte entfernen
-   
+    const LandAlle = [];
+    eingesetzte.map(eingesetzt => LandAlle[LandAlle.length] = eingesetzt.state);
+    // Duplicates of states are removed.
     const uniqueLand = [...new Set(LandAlle)];
-
-    var landKurz= uniqueLand;
-   
-
-
+    var landKurz = uniqueLand;
+    // Sort Länder.
     landKurz.sort(function(a, b) {
       return a - b;
     });
-
-      let isFiltered = false;
-      for (const value in checkedLand) {
-          if (checkedLand[value] === true) {
-            isFiltered = true;
-          }
+    let isFiltered = false;
+    for (const value in checkedLand) {
+      if (checkedLand[value] === true) {
+        isFiltered = true;
       }
-      // if (landFilter.length !== 0) {
-      if (isFiltered) {
-      landKurz = landKurz.filter( function (a) {
-        return checkedLand[a]==true;
-        // landFilter.indexOf (a) >= 0;
-       }
-      );
-      }
-
-      setLandGefiltert(landKurz);
+    }
+    //If there is a filter for Land, the following should happen: Each land included in "landkurz" is removed if it is not contained in the Filter as well.
+    if (isFiltered) {
+      landKurz = landKurz.filter(function (a) {
+        return checkedLand[a] === true;
+      });
+    }
+    setLandGefiltert(landKurz);
   }
 
-
   function applyVerfahrenFilter() {
-    const VerfahrenAlle=[];
-    eingesetzte.map(eingesetzt => VerfahrenAlle[VerfahrenAlle.length] = eingesetzt.service) ;
-    // doppelte entfernen
-   
+    const VerfahrenAlle = [];
+    eingesetzte.map(eingesetzt => VerfahrenAlle[VerfahrenAlle.length] = eingesetzt.service);
+    // Duplicates of services are removed.
     const uniqueVerfahren = [...new Set(VerfahrenAlle)];
-
-    var verfahrenKurz= uniqueVerfahren;
-   
+    var verfahrenKurz = uniqueVerfahren;
+    // Sort Services.
     verfahrenKurz.sort(function(a, b) {
       return a - b;
     });
-
-
-
-      if (verfahrenFilter.length !== 0) {
-        verfahrenKurz = verfahrenKurz.filter( function (a) {
+    // If there is a filter for services selected, the following should happen: Each service included in "verfahrenkurz" is removed if it is not contained in the Filter as well.
+    if (verfahrenFilter.length !== 0) {
+      verfahrenKurz = verfahrenKurz.filter( function (a) {
         return verfahrenFilter.indexOf (a) >= 0;
       });
       setVerfahrenGefiltert(verfahrenKurz);
@@ -119,34 +105,28 @@ function Ers() {
     }
   }
 
-function compare( a, b ) {
-      if ( a.release.toLowerCase()< b.release.toLowerCase()){
-        return -1;
-      }
-      if ( a.release.toLowerCase() > b.release.toLowerCase()){
-        return 1;
-      }
-      return 0;
+  //Function used later to sort Releases in the Table cells.
+  function compare(a, b) {
+    if (a.release.toLowerCase() < b.release.toLowerCase()) {
+      return -1;
     }
-
+    if (a.release.toLowerCase() > b.release.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  }
 
   useEffect(() => {
-
-    eingesetzte.sort( compare );
+    eingesetzte.sort(compare);
     applyLandFilter();
     applyVerfahrenFilter();
-
  }, [landFilter, verfahrenFilter, eingesetzte]);
 
- console.log(landFilter);
- console.log(checkedServices);
- console.log(verfahrenFilter);
 
-  
-      return (
-        <div>
-          <h1>Eingesetzte Releases</h1>
-          <ERFilter
+  return (
+    <div>
+      <h1>Eingesetzte Releases</h1>
+        <ERFilter
           eingesetzte={eingesetzte} 
           landFilter={landFilter} setLandFilter={setLandFilter} 
           verfahrenFilter={verfahrenFilter} setVerfahrenFilter={setVerfahrenFilter}
@@ -154,13 +134,13 @@ function compare( a, b ) {
           checkedServices={checkedServices} setCheckedServices={setCheckedServices}
           show={show} setShow={setShow}
           typeFilter={typeFilter} setTypeFilter={setTypeFilter}
-          />
-          <p></p>
-          <ERTable
+        />
+        <p></p>
+        <ERTable
           eingesetzte={eingesetzte} landGefiltert={landGefiltert} verfahrenGefiltert={verfahrenGefiltert} />
-        </div>
-      );
+    </div>
+  );
 }
   
-  export default Ers;
+export default Ers;
 
