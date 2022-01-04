596a283f73df23263866b3c34d650f162c5f020c
Robin Schomburg <robin.schomburg@hzd.hessen.de>
2022-01-04 16:17:00 +0100

    Cursive archived deployments.

--- modules/custom/hzd_react/js/src/components/ReleaseTable/DeployedReleasesTableRow.jsx
+++ modules/custom/hzd_react/js/src/components/ReleaseTable/DeployedReleasesTableRow.jsx
@@ -2,6 +2,9 @@
 import React, { useRef, useState, useEffect } from 'react'
 import { Popover, Overlay } from 'react-bootstrap';
 
+/**
+ * The DeployedReleasesTableRow React component.
+ */
 export default function DeployedReleasesTableRow({ deployment, detail }) {
   // Holds the popover text.
   const [info, setInfo] = useState(false);
@@ -21,95 +24,182 @@ export default function DeployedReleasesTableRow({ deployment, detail }) {
   // Show popover after ajax call finished loading data.
   useEffect(() => {
     if (detail) {
+      // Do nothing if detailed table is active.
       return;
     }
     if (info) {
+      // Show popover, if info text is available (loaded). Prevents alignment
+      // issues, when popover is shown before the info-text has been loaded.
       setShow(true);
     }
   }, [info])
 
-const handleInfo = () => {
-  if (show) {
+  /**
+   * Handles info-icon press. Toggles Info Popover.
+   */
+  const handleInfo = () => {
+    if (show) {
+      setShow(false);
+    }
+    if (!info) {
+      fetch('/ajaxnode/deployed/' + deployment.nid)
+        .then(response => response.json())
+        .then(result => {
+          setInfo(<div dangerouslySetInnerHTML={{ __html: result[1].data }} />);
+        })
+        .catch(error => console.log(error));
+    }
+    else if (!show) {
+      setShow(true);
+    }
+  }
+
+  /**
+   * Handles closing of the info-popover.
+   */
+  const hide = () => {
     setShow(false);
   }
-  if (!info) {
-    fetch('/ajaxnode/deployed/' + deployment.nid)
-      .then(response => response.json())
-      .then(result => {
-        setInfo(<div dangerouslySetInnerHTML={{ __html: result[1].data }} />);
-      })
-      .catch(error => console.log(error));
-  }
-  else if (!show) {
-    setShow(true);
-  }
-}
 
-const hide = () => {
-  setShow(false);
-}
-
-if (!detail) {
-  return (
-    <tr>
-      <td>{global.drupalSettings.states[deployment.state]}</td>
-      <td>{global.drupalSettings.environments[deployment.environment]}</td>
-      <td>{deployment.service}</td>
-      <td>{deployment.release}</td>
-      <td>{localeDate}</td>
-      <td>
-        <img
-          ref={target}
-          title="Einsatzinformationen anzeigen"
-          className="deployed-info-icon deployed-tooltip"
-          src="/modules/custom/hzd_release_management/images/notification-icon.png"
-          onClick={handleInfo}>
-        </img>
-        <Overlay
-          show={show}
-          target={target.current}
-          placement="left"
-          rootClose
-          onHide={hide}
-        >
-          <Popover id="popover-contained" title="Einsatzinformationen" className="margin-popover">
-            {info}
-          </Popover>
-        </Overlay>
-        <div className="other-links">
-        {deployment.downloadLink &&
-          <a href={deployment.downloadLink}><img title="Release herunterladen" src="/modules/custom/hzd_release_management/images/download_icon.png" />&nbsp;</a> 
+  if (!detail) {
+    // Returns the default view.
+    return (
+      <tr>
+        {deployment.status === "1" &&
+          <td>{global.drupalSettings.states[deployment.state]}</td>
         }
-        {deployment.docuLink &&
-          <a href={"/release-management/releases/documentation/" + deployment.serviceNid + "/" + deployment.releaseNid }><img title="Dokumentation ansehen" src="/modules/custom/hzd_release_management/images/document-icon.png" /></a>
+        {deployment.status === "2" &&
+          <td><i>{global.drupalSettings.states[deployment.state]}</i></td>
         }
-        </div>
-      </td>
-    </tr>
-  );
-}
-else {
-  return (
-    <tr>
-      <td>{global.drupalSettings.states[deployment.state]}</td>
-      <td>{global.drupalSettings.environments[deployment.environment]}</td>
-      <td>{deployment.release}</td>
-      <td>{localeDate}</td>
-      {deployment.previousRelease &&
-        <td>{deployment.previousRelease}</td>
-      }
-      {!deployment.previousRelease &&
-        <td>Ersteinsatz</td>
-      }
-      {deployment.installationTime && 
-        <td>{deployment.installationTime} Min</td>
-      }
-      {!deployment.installationTime &&
-        <td></td>
-      }
-      <td>{["Nein", "Ja"][deployment.automatedDeployment]}</td>
-      <td>{deployment.abnormalityDescription}</td>
-    </tr>
-  );
-}
+        {deployment.status === "1" &&
+          <td>{global.drupalSettings.environments[deployment.environment]}</td>
+        }
+        {deployment.status === "2" &&
+          <td><i>{global.drupalSettings.environments[deployment.environment]}</i></td>
+        }
+        {deployment.status === "1" &&
+          <td>{deployment.service}</td>
+        }
+        {deployment.status === "2" &&
+          <td><i>{deployment.service}</i></td>
+        }
+        {deployment.status === "1" &&
+          <td>{deployment.release}</td>
+        }
+        {deployment.status === "2" &&
+          <td><i>{deployment.release}</i></td>
+        }
+        {deployment.status === "1" &&
+          <td>{localeDate}</td>
+        }
+        {deployment.status === "2" &&
+          <td><i>{localeDate}</i></td>
+        }
+        <td>
+          <img
+            ref={target}
+            title="Einsatzinformationen anzeigen"
+            className="deployed-info-icon deployed-tooltip"
+            src="/modules/custom/hzd_release_management/images/notification-icon.png"
+            onClick={handleInfo}>
+          </img>
+          <Overlay
+            show={show}
+            target={target.current}
+            placement="left"
+            rootClose
+            onHide={hide}
+          >
+            <Popover id="popover-contained" title="Einsatzinformationen" className="margin-popover">
+              {info}
+            </Popover>
+          </Overlay>
+          <div className="other-links">
+          {deployment.downloadLink &&
+            <a href={deployment.downloadLink}><img title="Release herunterladen" src="/modules/custom/hzd_release_management/images/download_icon.png" />&nbsp;</a>
+          }
+          {deployment.docuLink &&
+            <a href={"/release-management/releases/documentation/" + deployment.serviceNid + "/" + deployment.releaseNid }><img title="Dokumentation ansehen" src="/modules/custom/hzd_release_management/images/document-icon.png" /></a>
+          }
+          </div>
+        </td>
+      </tr>
+    );
+  }
+  else {
+    // Returns the detailed view ("Einsatzinformationen").
+    return (
+      <tr>
+        {deployment.status === "1" &&
+          <td>{global.drupalSettings.states[deployment.state]}</td>
+        }
+        {deployment.status === "2" &&
+          <td><i>{global.drupalSettings.states[deployment.state]}</i></td>
+        }
+        {deployment.status === "1" &&
+          <td>{global.drupalSettings.environments[deployment.environment]}</td>
+        }
+        {deployment.status === "2" &&
+          <td><i>{global.drupalSettings.environments[deployment.environment]}</i></td>
+        }
+        {deployment.status === "1" &&
+          <td>{deployment.release}</td>
+        }
+        {deployment.status === "2" &&
+          <td><i>{deployment.release}</i></td>
+        }
+        {deployment.status === "1" &&
+          <td>{localeDate}</td>
+        }
+        {deployment.status === "2" &&
+          <td><i>{localeDate}</i></td>
+        }
+        {deployment.previousRelease &&
+        <td>
+          {deployment.status === "1" &&
+            deployment.previousRelease
+          }
+          {deployment.status === "2" &&
+            <i>{deployment.previousRelease}</i>
+          }
+        </td>
+        }
+        {!deployment.previousRelease &&
+          <td>
+            {deployment.status === "1" &&
+              "Ersteinsatz"
+            }
+            {deployment.status === "2" &&
+              <i>Ersteinsatz</i>
+            }
+          </td>
+        }
+        {deployment.installationTime && 
+          <td>
+            {deployment.status === "1" &&
+              deployment.installationTime + " min"
+            }
+            {deployment.status === "2" &&
+              <i>{deployment.installationTime} min</i>
+            }
+          </td>
+        }
+        {!deployment.installationTime &&
+          <td></td>
+        }
+        {deployment.status === "1" &&
+          <td>{["Nein", "Ja"][deployment.automatedDeployment]}</td>
+        }
+        {deployment.status === "2" &&
+          <td><i>{["Nein", "Ja"][deployment.automatedDeployment]}</i></td>
+        }
+        {deployment.status === "1" &&
+          <td>{deployment.abnormalityDescription}</td>
+        }
+        {deployment.status === "2" &&
+          <td><i>{deployment.abnormalityDescription}</i></td>
+        }
+      </tr>
+    );
+  }
 }
